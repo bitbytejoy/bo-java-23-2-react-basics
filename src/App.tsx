a3363b94cede96a02856d062914b700f996ca3f8
bitbytejoy <bitbytejoy@gmail.com>
2023-07-25 16:29:09 +0200

    BIG BANG

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Hello() {
+    return <h1>Hello world</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <Hello/>
+        </div>
+    );
+}
+
+export default App
